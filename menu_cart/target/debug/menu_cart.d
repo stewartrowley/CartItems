c74bf06ab0c9908eb56b13d97761e28d9593e884
Stewart Rowley <78276704+stewartrowley@users.noreply.github.com>
2022-03-21 20:38:39 -0600

    updtae

--- menu_cart/target/debug/menu_cart.d
+++ menu_cart/target/debug/menu_cart.d
@@ -1,1 +1,1 @@
-C:\Users\StewM\Documents\Stuff\myProjects\Rust\menu_cart\menu_cart\target\debug\menu_cart.exe: C:\Users\StewM\Documents\Stuff\myProjects\Rust\menu_cart\menu_cart\src\main.rs
+C:\Users\StewM\Documents\Stuff\myProjects\Rust\menu_cart\CartItems\menu_cart\target\debug\menu_cart.exe: C:\Users\StewM\Documents\Stuff\myProjects\Rust\menu_cart\CartItems\menu_cart\src\main.rs
